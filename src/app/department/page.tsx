4a1661ba3536a303bdd9bffd48585448194cabb7
Dardnos <adne.o94@gmail.com>
2024-03-19 11:03:50 +0100

    Add department page and link to department page from employee page

---
+++ src/app/department/page.tsx
@@ -0,0 +1,131 @@
+
+export default function Page() {
+    return (
+        <main>
+            <div className="flex justify-center mt-4">
+                <div className="overflow-x-auto">
+                    <table className="table my-4">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                {/* <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th> */}
+                                <th >Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                {/* <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th> */}
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div>
+                                            <div className="font-bold">Hart Hagerty</div>
+                                            <div className="text-sm opacity-50">United States</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Zemlak, Daniel and Leannon
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                </td>
+                                <td>Purple</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                            {/* row 2 */}
+                            <tr>
+                                {/* <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th> */}
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div>
+                                            <div className="font-bold">Brice Swyre</div>
+                                            <div className="text-sm opacity-50">China</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Carroll Group
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Tax Accountant</span>
+                                </td>
+                                <td>Red</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                            {/* row 3 */}
+                            <tr>
+                                {/* <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th> */}
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div>
+                                            <div className="font-bold">Marjy Ferencz</div>
+                                            <div className="text-sm opacity-50">Russia</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Rowe-Schoen
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Office Assistant I</span>
+                                </td>
+                                <td>Crimson</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                            {/* row 4 */}
+                            <tr>
+                                {/* <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th> */}
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div>
+                                            <div className="font-bold">Yancy Tear</div>
+                                            <div className="text-sm opacity-50">Brazil</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Wyman-Ledner
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+                                </td>
+                                <td>Indigo</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                        </tbody>
+                        {/* foot */}
+                        <tfoot></tfoot>
+                    </table>
+                </div>
+            </div>
+        </main>
+    );
+}
--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -1,4 +1,15 @@
+import Link from "next/link";
+
 // `app/page.tsx` is the UI for the `/` URL
 export default function Page() {
-    return (<h1>Hello, Employee home page!</h1>);
+    return (<><h1>Hello, Employee home page!</h1><main className="container mx-auto flex flex-row items-start justify-center mt-20 space-x-4">
+      <Link
+        href="/department"
+        className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+      >
+        Go to Departments
+      </Link>
+    </main></>
+      );
   }
+

d8743c18a4e368926c342d463754cc200206380c
Johakv <johakv@stud.ntnu.no>
2024-03-19 13:49:15 +0100

    Made more dummy pages to be filled in later

---
+++ src/app/admin/administerStudies/page.tsx
@@ -0,0 +1,6 @@
+/** @format */
+
+// `app/page.tsx` is the UI for the `/` URL
+export default function Page() {
+  return <h1>Hello, administerStudies page!</h1>;
+}
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,32 +2,32 @@
 
 import Link from "next/link";
 export default function Page() {
-	return (
-		<main className="container mx-auto flex flex-row items-start justify-center mt-20 space-x-4">
-			<Link
-				href="/coordinator/orderInternships"
-				className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
-			>
-				Administrer Studier
-			</Link>
-			<Link
-				href="/coordinator/administerStudents"
-				className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
-			>
-				Administrer Lærere
-			</Link>
-			<Link
-				href="/coordinator/administerStudents"
-				className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
-			>
-				Administrer Ansatte
-			</Link>
-			<Link
-				href="/coordinator/administerStudents"
-				className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
-			>
-				Administrer Avdelinger
-			</Link>
-		</main>
-	);
+  return (
+    <main className="container mx-auto flex flex-row items-start justify-center mt-20 space-x-4">
+      <Link
+        href="/admin/administerS"
+        className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+      >
+        Administrer Studier
+      </Link>
+      <Link
+        href="/admin/administerTeachers"
+        className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+      >
+        Administrer Lærere
+      </Link>
+      <Link
+        href="/admin/administerEmployees"
+        className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+      >
+        Administrer Ansatte
+      </Link>
+      <Link
+        href="/admin/administerDepartments"
+        className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+      >
+        Administrer Avdelinger
+      </Link>
+    </main>
+  );
 }

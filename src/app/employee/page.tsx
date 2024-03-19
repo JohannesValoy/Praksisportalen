4255aea9f537c52847f9098500ef14d2e3951f97
Johakv <johakv@stud.ntnu.no>
2024-03-19 13:18:35 +0100

    formatting

--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -2,14 +2,17 @@ import Link from "next/link";
 
 // `app/page.tsx` is the UI for the `/` URL
 export default function Page() {
-    return (<><h1>Hello, Employee home page!</h1><main className="container mx-auto flex flex-row items-start justify-center mt-20 space-x-4">
-      <Link
-        href="/department"
-        className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
-      >
-        Go to Departments
-      </Link>
-    </main></>
-      );
-  }
-
+  return (
+    <>
+      <h1>Hello, Employee home page!</h1>
+      <main className="container mx-auto flex flex-row items-start justify-center mt-20 space-x-4">
+        <Link
+          href="/department"
+          className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+        >
+          Go to Departments
+        </Link>
+      </main>
+    </>
+  );
+}

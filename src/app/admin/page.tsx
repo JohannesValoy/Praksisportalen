05e457e8d4228e3d9f358fa5b28bf72ddbc63534
Johakv <johakv@stud.ntnu.no>
2024-03-19 14:09:02 +0100

    made test email show in a button for testing

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,20 @@
 /** @format */
 
 import Link from "next/link";
-export default function Page() {
+import prisma from "../module/prismaClient";
+export default async function Page() {
+  const user = await prisma.candidate.findUnique({
+    where: {
+      id: "1",
+    },
+  });
   return (
     <main className="container mx-auto flex flex-row items-start justify-center mt-20 space-x-4">
       <Link
         href="/admin/administerS"
         className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
       >
+        <h1>{user?.email}</h1>
         Administrer Studier
       </Link>
       <Link

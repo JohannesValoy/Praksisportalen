2d8e5b91a29ceaaa8173c79e5c22e4b2aed5a4a1
Dardnos <adne.o94@gmail.com>
2024-03-20 20:01:47 +0100

    Refactor table structure in ListOfStudents component

--- src/app/components/listOfStudents.tsx
+++ src/app/components/listOfStudents.tsx
@@ -11,10 +11,11 @@ const ListOfDepartments = async () => {
       <div className="flex justify-center mt-4">
         <div className="overflow-x-auto w-full p-4">
           <h1 className="text-3xl font-semibold">List of Users</h1>
-          {users.map((user, index) => (
-            <div key={index}>
-              <table className="table my-4">
-                <tbody>
+
+          <div>
+            <table className="table my-4">
+              {users.map((user, index) => (
+                <tbody key={index}>
                   <tr>
                     <td>
                       <div className="flex items-center gap-3">
@@ -32,22 +33,16 @@ const ListOfDepartments = async () => {
                         </div>
                       </div>
                     </td>
-                    <td>
-                      {user?.email}
-                      <br />
-                      <span className="badge badge-ghost badge-sm">
-                        {user?.id}
-                      </span>
-                    </td>
-                    <td>{user?.createdAt?.getDate()?.toString()}</td>
+                    <td>{user?.email}</td>
+                    <td>{user?.createdAt.toLocaleDateString()}</td>
                     <th>
                       <button className="btn btn-ghost btn-xs">details</button>
                     </th>
                   </tr>
                 </tbody>
-              </table>
-            </div>
-          ))}
+              ))}
+            </table>
+          </div>
         </div>
       </div>
     </div>

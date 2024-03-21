3fdd8b3b6ecb0c67e5fdec3964e6e0ce820a780a
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-03-21 14:02:46 +0100

    made the datalist for the timeline come from the student page, working on making it come from the database

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -1,11 +1,20 @@
 /** @format */
+
 "use client";
-// `app/page.tsx` is the UI for the `/` URL
+import dynamic from "next/dynamic";
+import prisma from "../module/prismaClient";
 import Gantt from "../components/Gantt";
+
 export default function Page() {
+  const datalist = [
+    ["Washington", new Date(1801, 3, 29), new Date(1827, 2, 3)],
+    ["Adams", new Date(1797, 2, 3), new Date(1801, 2, 3)],
+    ["Jefferson", new Date(1801, 2, 3), new Date(1809, 2, 3)],
+  ];
+
   return (
     <div className="flex flex-row gap-20 w-full h-full items-center justify-center">
-      <Gantt />
+      <Gantt datalist={datalist} />
       <div className="flex flex-col gap-5 items-center justify-center">
         <div
           style={{

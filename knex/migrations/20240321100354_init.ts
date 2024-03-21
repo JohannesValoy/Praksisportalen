a3bd745448d2415c57528d954e4224c64e5f6b51
JeyKey09 <mathias-jk@live.no>
2024-03-21 15:32:53 +0100

    Missing getting seed to work

--- knex/migrations/20240321100354_init.ts
+++ knex/migrations/20240321100354_init.ts
@@ -78,17 +78,19 @@ export async function up(knex: Knex): Promise<void> {
       table.string("status").notNullable();
       table.date("startDate").notNullable();
       table.date("endDate").notNullable();
-      table.integer("student_id").unsigned();
+      table.integer("student_id").unsigned().notNullable();
       table
         .foreign("student_id")
         .references("id")
         .inTable("users")
         .onDelete("CASCADE");
-<<<<<<<< HEAD:src/knex/migrations/20240321100354_init.ts
       table.integer("coordinator_id").unsigned();
       table.foreign("coordinator_id").references("id").inTable("users");
       table.integer("studyProgram_id").unsigned().notNullable();
-      table.foreign("studyProgram_id").references("id").inTable("studyProgram");
+      table
+        .foreign("studyProgram_id")
+        .references("id")
+        .inTable("studyPrograms");
       table.integer("internship_id").unsigned().notNullable();
       table
         .foreign("internship_id")
@@ -106,3 +108,6 @@ export async function up(knex: Knex): Promise<void> {
       table.timestamp("created_at").defaultTo(knex.fn.now());
       table.timestamp("updated_at").defaultTo(knex.fn.now());
     });
+}
+
+export async function down(knex: Knex): Promise<void> {}

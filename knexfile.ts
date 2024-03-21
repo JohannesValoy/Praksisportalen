1551ca6989aa2c626f828024eb414211923744a3
JeyKey09 <mathias-jk@live.no>
2024-03-21 16:18:38 +0100

    moved next for easier import for other libs

--- knexfile.ts
+++ knexfile.ts
@@ -17,13 +17,13 @@ export const config: { [key: string]: Knex.Config } = {
       uri: env.DATABASE_URL,
     },
     migrations: {
-      directory: path.join(__dirname, "knex/migrations"),
+      directory: path.join(__dirname, "src/knex/migrations"),
       extension: extension,
       tableName: "knex_migrations",
       loadExtensions: [`.${extension}`],
     },
     seeds: {
-      directory: path.join(__dirname, "knex/seeds"),
+      directory: path.join(__dirname, "src/knex/seeds"),
     }
   },
 

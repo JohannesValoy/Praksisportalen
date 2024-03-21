137c96cfc354f24ee4fa45ddfb30e358484561ee
JeyKey09 <mathias-jk@live.no>
2024-03-21 12:35:07 +0100

    Tables for typescript

---
+++ src/app/_knex/tables.tsx
@@ -0,0 +1,26 @@
+import { Knex } from "knex";
+
+declare module "knex/types/tables" {
+  interface User {
+      id: number;
+      email: string;
+      password: string;
+      created_at: Date;
+      updated_at: Date;
+  }
+
+    interface Departments {
+        departments: {
+        id: number;
+        name: string;
+        created_at: Date;
+        updated_at: Date;
+        };
+    }
+
+    interface Tables {
+        users: User;
+        departments: Departments;
+
+    }
+}

4ad5e32cd92e7b4ba1994f738888e287bd1503f6
Dardnos <adne.o94@gmail.com>
2024-03-22 15:33:34 +0100

    Add department and section routes and update department and section pages

---
+++ src/app/department/section/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import type { Section } from "knex/types/tables.js";
+import { use, useEffect, useState } from "react";
+
+/** @format */
+export default function Page() {
+  const [sections, setsections] = useState<Section[]>([]);
+  useEffect(() => {
+    fetch("/api/sections").then((res) => res.json().then(setsections));
+  }, []);
+
+  return (
+    <main>
+      <h1>Sections</h1>
+      <ul>
+        {sections?.map((section) => (
+          <li key={section.id}>{section.name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
--- src/knex/config/tables.ts
+++ src/knex/config/tables.ts
@@ -6,13 +6,7 @@ declare module "knex/types/tables.js" {
     name: string;
     email: string;
     password: string;
-    created_at: Date;
-    updated_at: Date;
-  }
-
-  interface Department {
-    id: number;
-    name: string;
+    role: string;
     created_at: Date;
     updated_at: Date;
   }
@@ -26,9 +20,74 @@ declare module "knex/types/tables.js" {
     created_at: Date;
     updated_at: Date;
   }
+
+  interface Department {
+    id: number;
+    name: string;
+    employee_id: number;
+    created_at: Date;
+    updated_at: Date;
+  }
+
+  interface Section {
+    id: number;
+    name: string;
+    type: string;
+    employee_id: number;
+    department_id: number;
+    created_at: Date;
+    updated_at: Date;
+  }
+
+  interface Internship {
+    id: number;
+    name: string;
+    field: string;
+    maxCapacity: number;
+    currentCapacity: number;
+    numberOfBeds: number;
+    yearOfStudy: number;
+    section_id: number;
+    created_at: Date;
+    updated_at: Date;
+  }
+
+  interface EducationInstitution {
+    id: number;
+    name: string;
+    created_at: Date;
+    updated_at: Date;
+  }
+
+  interface StudyProgram {
+    id: number;
+    name: string;
+    educationInstitution_id: number;
+    created_at: Date;
+    updated_at: Date;
+  }
+
+  interface internshipAgreements {
+    id: number;
+    status: string;
+    startDate: Date;
+    endDate: Date;
+    student_id: number;
+    internship_id: number;
+    studyProgram_id: number;
+    comment: string;
+    created_at: Date;
+    updated_at: Date;
+  }
+
   interface Tables {
     users: User;
     departments: Department;
     accounts: Account;
+    sectors: Section;
+    internships: Internship;
+    educationInstitutions: EducationInstitution;
+    studyPrograms: StudyProgram;
+    internshipAgreements: internshipAgreements;
   }
 }

8f66db6809e85613e4904b346bce17f8d5fb28fe
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-03-25 14:35:13 +0100

    added add section view, it works as intended, but you can not search for employees or departments, and have to scroll down to find

--- src/app/api/sections/route.ts
+++ src/app/api/sections/route.ts
@@ -1,6 +1,21 @@
+/** @format */
+
 import DBclient from "@/knex/config/DBClient";
 
 export async function GET(request: Request) {
   const sections = await DBclient.from("sections").select("*");
   return Response.json(sections);
+}
+
+export async function POST(request: Request) {
+  const { name, sectionType, employee_id, department_id } =
+    await request.json();
+  const newSection = await DBclient.table("sections").insert({
+    name,
+    type: sectionType,
+    department_id,
+    employee_id,
+  });
+  console.log(newSection);
+  return Response.json(newSection);
 }

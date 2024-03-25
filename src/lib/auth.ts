2c20adfdd4dd1fb7bd0ceeef4c48f6865ee3a5fc
Dardnos <adne.o94@gmail.com>
2024-03-25 13:01:31 +0100

    Update user roles in auth.ts, employee was missing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,21 +2,23 @@ import DBclient from "@/knex/config/DBClient";
 import { getSession } from "next-auth/react";
 
 export enum Role {
-    STUDENT = "student",
-    COORDINATOR = "coordinator",
-    ADMIN = "admin",
-    NONE = "none",
+  STUDENT = "student",
+  COORDINATOR = "coordinator",
+  EMPLOYEE = "employee",
+  ADMIN = "admin",
+  NONE = "none",
 }
 export async function checkUserRole(): Promise<Role> {
-    await getSession().then((session) => {
-        if (session) {
-            return DBclient.from("users")
-                .select("role")
-                .where("email", session.user.email)
-                .first().then((user) => {
-                    return Role[user.role] || Role.NONE;
-                });
-        }
-    });
-    return Role.NONE; // Add this line to return a default value
+  await getSession().then((session) => {
+    if (session) {
+      return DBclient.from("users")
+        .select("role")
+        .where("email", session.user.email)
+        .first()
+        .then((user) => {
+          return Role[user.role] || Role.NONE;
+        });
+    }
+  });
+  return Role.NONE; // Add this line to return a default value
 }

215b227d49c61462c20b834a79d32859db966288
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-03-25 15:05:56 +0100

    now add Study program and add education institution works

--- src/app/api/studyPrograms/route.ts
+++ src/app/api/studyPrograms/route.ts
@@ -6,3 +6,13 @@ export async function GET(request: Request) {
   const studyPrograms = await DBclient.from("studyPrograms").select("*");
   return Response.json(studyPrograms);
 }
+
+export async function POST(request: Request) {
+  const { name, educationInstitution_id } = await request.json();
+  const newStudyProgram = await DBclient.table("studyPrograms").insert({
+    name,
+    educationInstitution_id,
+  });
+  console.log(newStudyProgram);
+  return Response.json(newStudyProgram);
+}

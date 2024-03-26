77a9789b4f5bfc6f4991e97c9a1b5df7873cd335
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-03-26 11:43:17 +0100

    added post internship function in api + fixed warning at student home view

--- src/app/api/internships/route.ts
+++ src/app/api/internships/route.ts
@@ -6,3 +6,23 @@ export async function GET(request: Request) {
   const internships = await DBclient.from("internships").select("*");
   return Response.json(internships);
 }
+
+export async function POST(request: Request) {
+  const {
+    name,
+    field,
+    currentCapacity,
+    numberOfBeds,
+    yearOfStudy,
+    section_id,
+  } = await request.json();
+  const internship = await DBclient("internships").insert({
+    name,
+    field,
+    currentCapacity,
+    numberOfBeds,
+    yearOfStudy,
+    section_id,
+  });
+  return Response.json(internship);
+}

18a8a1b3faa62df29241ba7679b0b2895376bb03
JeyKey09 <mathias-jk@live.no>
2024-03-26 14:32:09 +0100

    Fixed error

--- src/knex/migrations/20240321100354_init.ts
+++ src/knex/migrations/20240321100354_init.ts
@@ -106,3 +106,5 @@ export async function up(knex: Knex): Promise<void> {
       table.timestamp("updated_at").defaultTo(knex.fn.now());
     });
   }
+  export async function down(knex: Knex): Promise<void> {
+}

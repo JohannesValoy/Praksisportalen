d003639e428b1542c95f03dfca5e80caed4f0366
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-03-26 17:19:57 +0100

    some red lines in internship view, TODO. Fixed sections and internship list emails

--- src/app/api/sections/[id]/route.ts
+++ src/app/api/sections/[id]/route.ts
@@ -1,21 +1,18 @@
 /** @format */
 
-import DBClient from "@/knex/config/DBClient";
-import SectionJson from "../SectionView";
+import DBclient from "@/knex/config/DBClient";
+import SectionView from "../SectionView";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: number } }
 ) {
-  // Fetch the section along with the user's email who is referred by employee_id
-  const sectionWithUserEmail = await DBClient.table("sections")
+  const section = await DBclient("sections")
     .where({ "sections.department_id": params.id })
     .join("users", "sections.employee_id", "=", "users.id")
-    .select("sections.*", "users.email as employee_email") // Selecting all from sections and user's email
-    .first(); // Assuming you're looking for a single section
-
-  if (sectionWithUserEmail) {
-    return Response.json(new SectionJson(sectionWithUserEmail));
+    .select("sections.*", "users.email as employee_email"); // Selecting all from sections and user's email
+  if (section) {
+    return Response.json(new SectionView(section[0]));
   }
-  return Response.json({ message: "User not found" }, { status: 404 });
+  return Response.json({ message: "Internship not found" }, { status: 404 });
 }

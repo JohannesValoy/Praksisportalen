df47f1ae4e641bbf3e2664f4feaf4856b14a2c7c
JeyKey09 <mathias-jk@live.no>
2024-03-26 16:50:00 +0100

    Lets make it pageable

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,15 +1,13 @@
 /** @format */
+import { PageResponse, UserPageRequest } from "@/app/_models/pageinition";
 import DBclient from "@/knex/config/DBClient";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("role")?.split(",") || [
-    "student",
-    "employee",
-    "coordinator",
-    "admin",
-  ];
-  const users = await DBclient.from("users").whereIn("role", query).select("*");
-  return Response.json(users);
+  const pageRequest = UserPageRequest.fromRequest(request);
+  const users = await DBclient.from("users")
+    .select("*")
+    .whereIn("role", pageRequest.roles)
+    .orderBy(pageRequest.sort);
+  return Response.json(new PageResponse(pageRequest, users));
 }

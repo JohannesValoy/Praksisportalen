0bed906aef3e897c5b907d40555e1e225fb665c9
Dardnos <adne.o94@gmail.com>
2024-03-26 16:12:57 +0100

    Add api route file for internshipFields

---
+++ src/app/api/internships/internshipFields/route.ts
@@ -0,0 +1,16 @@
+/** @format */
+
+import DBclient from "@/knex/config/DBClient";
+
+export async function GET(request: Request) {
+  const internshipFields = await DBclient.from("internshipFields").select("*");
+  return Response.json(internshipFields);
+}
+
+export async function POST(request: Request) {
+  const { name } = await request.json();
+  const newInternshipField = await DBclient.table("internshipFields").insert({
+    name,
+  });
+  return Response.json(newInternshipField);
+}

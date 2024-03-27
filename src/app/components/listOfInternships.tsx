e9083e0dd655f9a8bddc0ccf82e32a0678455685
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-03-27 12:21:45 +0100

    added some error handling- but should ideally be done in a better way. working on add internship

--- src/app/components/listOfInternships.tsx
+++ src/app/components/listOfInternships.tsx
@@ -5,6 +5,7 @@
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { Internship } from "knex/types/tables.js";
 
 const ListOfInternships = () => {
   const searchParams = useSearchParams();
@@ -15,13 +16,14 @@ const ListOfInternships = () => {
     if (id !== null) {
       fetch(`/api/internships/${id}`)
         .then((res) => res.json())
-        .then((data) => setInternships([data]));
+        .then((data) => setInternships(data));
     } else {
       fetch(`/api/internships`)
         .then((res) => res.json())
         .then(setInternships);
     }
   }, [id]);
+  console.log(internships);
 
   type Internship = {
     name: string;
@@ -47,50 +49,66 @@ const ListOfInternships = () => {
                 <th>Email</th>
                 <th>ID</th>
                 <th>
-                  <Link
-                    href="/"
-                    className="inline-block px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+                  <a
+                    href={`/admin/administerInternships/addInternship`}
+                    className="btn btn-xs"
                   >
                     Add Internship
-                  </Link>
+                  </a>
                 </th>
               </tr>
             </thead>
-            {internships.map((internship, index) => (
-              <tbody key={index}>
+
+            {Array.isArray(internships) &&
+            internships.filter(
+              (internship) =>
+                internship.name &&
+                internship.id &&
+                internship.employee_id &&
+                internship.employee_email
+            ).length > 0 ? (
+              internships.map((internship, index) => (
+                <tbody key={index}>
+                  <tr>
+                    <th>
+                      <label>
+                        <input type="checkbox" className="checkbox" />
+                      </label>
+                    </th>
+                    <th>
+                      <div className="font-bold">{internship?.name}</div>
+                    </th>
+                    <td>
+                      <button
+                        className="btn btn-ghost btn-xs"
+                        onClick={() => {
+                          window.location.href = `/profile/?id=${internship.employee_id}`;
+                        }}
+                      >
+                        {internship?.employee_email}
+                      </button>
+                    </td>
+                    <td>{internship?.id}</td>
+                    <th>
+                      <button
+                        className="btn btn-ghost btn-xs"
+                        onClick={() => {
+                          window.location.href = `/admin/administerInternships/individualInternship?internship_id=${internship.id}`;
+                        }}
+                      >
+                        details
+                      </button>
+                    </th>
+                  </tr>
+                </tbody>
+              ))
+            ) : (
+              <tbody>
                 <tr>
-                  <th>
-                    <label>
-                      <input type="checkbox" className="checkbox" />
-                    </label>
-                  </th>
-                  <th>
-                    <div className="font-bold">{internship?.name}</div>
-                  </th>
-                  <td>
-                    <button
-                      className="btn btn-ghost btn-xs"
-                      onClick={() => {
-                        window.location.href = `/profile/?id=${internship.employee_id}`;
-                      }}
-                    >
-                      {internship?.employee_email}
-                    </button>
-                  </td>
-                  <td>{internship?.id}</td>
-                  <th>
-                    <button
-                      className="btn btn-ghost btn-xs"
-                      onClick={() => {
-                        window.location.href = `/admin/administerInternships/individualInternship?internship_id=${internship.id}`;
-                      }}
-                    >
-                      details
-                    </button>
-                  </th>
+                  <td colSpan={5}>Empty</td>
                 </tr>
               </tbody>
-            ))}
+            )}
           </table>
         </div>
       ) : (

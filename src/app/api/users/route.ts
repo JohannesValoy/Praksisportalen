af490443a8f137f8c9839128074c370fe7442165
JeyKey09 <mathias-jk@live.no>
2024-04-09 11:41:14 +0200

    rework of user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,7 +2,7 @@
 import { PageResponse, UserPageRequest } from "@/app/_models/pageinition";
 import DBclient from "@/knex/config/DBClient";
 import { NextRequest } from "next/server";
-
+//FIXME: Does not work
 export async function GET(request: NextRequest) {
   const pageRequest = UserPageRequest.fromRequest(request);
   const users = await DBclient.from("users")
@@ -10,4 +10,20 @@ export async function GET(request: NextRequest) {
     .whereIn("role", pageRequest.roles)
     .orderBy(pageRequest.sort);
   return Response.json(new PageResponse(pageRequest, users));
+}
+
+export async function POST(request: NextRequest) {
+  const { firstName, lastName, email, phoneNumber, role } =
+    await request.json();
+
+  const newUser = await DBclient.table("users").insert({
+    name: firstName + " " + lastName,
+    //lastName, //to be implemented in the server
+    email,
+    password: "123456", //should probably be auto-generated and return to a message to the user with the password
+    //phoneNumber, //to be implemented in the server
+    role,
+  });
+
+  return Response.json(newUser);
 }

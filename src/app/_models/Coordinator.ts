180e4fceca87e75326165e95343acea2a8ff2af3
JeyKey09 <mathias-jk@live.no>
2024-04-10 15:16:42 +0200

    Coord and employee pagerequest

--- src/app/_models/Coordinator.ts
+++ src/app/_models/Coordinator.ts
@@ -1,4 +1,6 @@
 import { CoordinatorTable } from "knex/types/tables.js";
+import { PageRequest } from "./pageinition";
+import { NextRequest } from "next/server";
 
 class Coordinator implements CoordinatorTable {
   id?: string;
@@ -27,3 +29,41 @@ class Coordinator implements CoordinatorTable {
     };
   }
 }
+
+class CoordinatorPageRequest extends PageRequest {
+  private _name;
+  private _email;
+
+  constructor(
+    page: number,
+    size: number,
+    sort: string = "id",
+    name: string = "",
+    email: string = ""
+  ) {
+    super(page, size);
+    if (["name", "email"].includes(sort)) {
+      this.sort = sort;
+    }
+    this._name = name;
+    this._email = email;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  static fromRequest(request: NextRequest) {
+    const base = super.fromRequest(request);
+    const name = request.nextUrl.searchParams.get("name");
+    const email = request.nextUrl.searchParams.get("email");
+    const sort = request.nextUrl.searchParams.get("sort");
+    return new CoordinatorPageRequest(base.page, base.size, sort, name, email);
+  }
+}
+
+export { Coordinator, CoordinatorPageRequest };

eded687b03c59f067bc39076a554227724645f52
JeyKey09 <mathias-jk@live.no>
2024-04-11 16:40:14 +0200

    Can now be built

--- src/app/api/internshipAgreements/route.ts
+++ src/app/api/internshipAgreements/route.ts
@@ -1,10 +1,13 @@
 /** @format */
-
 import DBclient from "@/knex/config/DBClient";
+import { NextRequest } from "next/server";
+//FIXME: This code can't be built without the following variables. Find out why
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const internshipAgreements = await DBclient.from(
-    "internshipAgreements",
+    "internshipAgreements"
   ).select("*");
   return Response.json(internshipAgreements);
 }

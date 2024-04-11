a373c3fc86f5219d3c3ae92712d29524e743eb47
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-04-11 11:32:18 +0200

    made me/route.ts return user object

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -4,7 +4,9 @@ import { getUser } from "@/lib/auth";
 
 export async function GET(request: NextRequest) {
   const user = await getUser();
-  if (!user) {
+  if (user) {
+    return { status: 200, body: user };
+  } else {
     return { status: 401, body: { message: "Unauthorized" } };
   }
 }

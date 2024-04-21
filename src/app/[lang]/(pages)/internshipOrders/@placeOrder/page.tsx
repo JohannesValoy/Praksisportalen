2c88cd0915ff2340186387bb062bc5af94068c66
Dardnos <adne.o94@gmail.com>
2024-04-21 14:51:20 +0200

    Remove unused dialog id in internshipOrders/@placeOrder/page.tsx

--- src/app/[lang]/(pages)/internshipOrders/@placeOrder/page.tsx
+++ src/app/[lang]/(pages)/internshipOrders/@placeOrder/page.tsx
@@ -170,7 +170,6 @@ export default function Page() {
   return (
     <div>
       <dialog
-        id="my_modal_5"
         open={isModalVisible}
         className="modal  modal-bottom sm:modal-middle"
       >

b4abc1d004bde72b5a52067c7ec4b6c1fe855b1e
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-04-23 12:31:22 +0200

    made updates to error handling

--- src/app/[lang]/(pages)/internshipOrders/@receivedOrders/actions.ts
+++ src/app/[lang]/(pages)/internshipOrders/@receivedOrders/actions.ts
@@ -23,72 +23,47 @@ export interface Order {
     };
   };
 }
+
 export async function fetchOrders(): Promise<Order[]> {
-  try {
-    const orders = await DBclient.table("internshipOrders")
-      .innerJoin(
-        "fieldGroups",
-        "internshipOrders.id",
-        "fieldGroups.internshipOrderID"
-      )
-      .innerJoin(
-        "subFieldGroups",
-        "fieldGroups.id",
-        "subFieldGroups.fieldGroupID"
-      )
-      .select("*");
-
-    if (orders.length === 0) {
-      throw new Error("No orders found in the internshipOrders table.");
-    }
-
-    const studyPrograms = await DBclient.table("studyPrograms")
-      .whereIn(
-        "studyPrograms.id",
-        orders.map((order) => order.studyProgram_id)
-      )
-      .select();
-
-    if (studyPrograms.length === 0) {
-      throw new Error("No study programs found for the given orders.");
-    }
-
-    const educationInstitutes = await DBclient.table("educationInstitutions")
-      .select()
-      .whereIn(
-        "id",
-        studyPrograms.map(
-          (studyProgram) => studyProgram.educationInstitution_id
-        )
-      );
-
-    if (educationInstitutes.length === 0) {
-      throw new Error(
-        "No education institutions found for the given study programs."
-      );
-    }
-
-    const response = orders.map((order) => {
-      const studyProgram = studyPrograms.find(
-        (studyProgram) => studyProgram.id === order.studyProgram_id
-      );
-      return {
-        ...order,
-        studyProgram: {
-          ...studyProgram,
-          educationInstitute: {
-            ...educationInstitutes.find(
-              (institute) =>
-                institute.id === studyProgram.educationInstitution_id
-            ),
-          },
+  const orders = await DBclient.table("internshipOrders")
+    .innerJoin(
+      "fieldGroups",
+      "internshipOrders.id",
+      "fieldGroups.internshipOrderID"
+    )
+    .innerJoin(
+      "subFieldGroups",
+      "fieldGroups.id",
+      "subFieldGroups.fieldGroupID"
+    )
+    .select("*");
+  const studyPrograms = await DBclient.table("studyPrograms")
+    .whereIn(
+      "studyPrograms.id",
+      orders.map((order) => order.studyProgram_id)
+    )
+    .select();
+  const educationInstitutes = await DBclient.table("educationInstitutions")
+    .select()
+    .whereIn(
+      "id",
+      studyPrograms.map((studyProgram) => studyProgram.educationInstitution_id)
+    );
+  const response = orders.map((order) => {
+    const studyProgram = studyPrograms.find(
+      (studyProgram) => studyProgram.id === order.studyProgram_id
+    );
+    return {
+      ...order,
+      studyProgram: {
+        ...studyProgram,
+        educationInstitute: {
+          ...educationInstitutes.find(
+            (institute) => institute.id === studyProgram.educationInstitution_id
+          ),
         },
-      };
-    });
-
-    return response;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+      },
+    };
+  });
+  return response;
 }
--- src/app/[lang]/(pages)/internshipOrders/@receivedOrders/page.tsx
+++ src/app/[lang]/(pages)/internshipOrders/@receivedOrders/page.tsx
@@ -23,32 +23,34 @@ export default function Page() {
       <ContainerBox title={"Received Orders"}>
         <div className="flex flex-col gap-5 overflow-hidden ">
           {orders ? (
-            [...orders].reverse().map((order) => (
-              <div key={order.id} className="collapse bg-base-200">
-                <input type="checkbox" />
-                <div className="collapse-title text-xl font-medium">
-                  {order.studyProgram.educationInstitute.name} /{" "}
-                  {order.studyProgram.name} / {order.internshipField} /{" "}
-                  {order.studyYear} / {order.created_at.toLocaleDateString()}{" "}
-                  (Click to open/close)
+            orders.length > 0 ? (
+              [...orders].reverse().map((order) => (
+                <div key={order.id} className="collapse bg-base-200">
+                  <input type="checkbox" />
+                  <div className="collapse-title text-xl font-medium">
+                    {order.studyProgram.educationInstitute.name} /{" "}
+                    {order.studyProgram.name} / {order.internshipField} /{" "}
+                    {order.studyYear} / {order.created_at.toLocaleDateString()}{" "}
+                    (Click to open/close)
+                  </div>
+                  <div className="collapse-content">
+                    <p>Number of students: {order.numStudents}</p>
+                    <p>Start week: {order.startWeek}</p>
+                    <p>End week: {order.endWeek}</p>
+                    <p>Comment: {order.comment.toString()}</p>
+                  </div>
                 </div>
-                <div className="collapse-content">
-                  <p>Number of students: {order.numStudents}</p>
-                  <p>Start week: {order.startWeek}</p>
-                  <p>End week: {order.endWeek}</p>
-                  <p>Comment: {order.comment.toString()}</p>
-                </div>
-              </div>
-            ))
-          ) : error ? (
-            error === "No orders found in the internshipOrders table." ? (
+              ))
+            ) : (
               <p>No orders found.</p>
-            ) : isModalOpen ? (
+            )
+          ) : error ? (
+            isModalOpen && (
               <ErrorModal
                 message={error}
                 setIsModalOpen={setIsModalOpen}
               ></ErrorModal>
-            ) : null
+            )
           ) : (
             <p>Loading...</p>
           )}

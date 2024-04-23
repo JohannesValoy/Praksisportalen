85e8602947f3c3dcd7201e77dbeddeecee6b02bb
Dardnos <adne.o94@gmail.com>
2024-04-23 14:42:46 +0200

    Update theme to HMR in ThemeContext.tsx

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -16,7 +16,7 @@ export const ThemeProvider = ({ children }: any) => {
     if (localTheme) {
       setTheme(localTheme);
     } else {
-      setTheme("light");
+      setTheme("HMR");
     }
     setThemeLoaded(true);
   }, []);

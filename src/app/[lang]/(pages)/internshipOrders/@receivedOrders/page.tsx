f5900dbf6fe4ec31de6dc3465061d51670f13e87
JohannesValoy <99797871+JohannesValoy@users.noreply.github.com>
2024-04-23 12:23:21 +0200

    made error modal universal

--- src/app/[lang]/(pages)/internshipOrders/@receivedOrders/page.tsx
+++ src/app/[lang]/(pages)/internshipOrders/@receivedOrders/page.tsx
@@ -1,40 +1,59 @@
+/** @format */
 "use client";
 import React, { useEffect, useState } from "react";
 import { Order, fetchOrders } from "./actions";
 import ContainerBox from "@/app/components/ContainerBox";
+import ErrorModal from "@/app/components/ErrorModal";
 
 export default function Page() {
   const [orders, setOrders] = useState<Order[]>(null);
+  const [error, setError] = useState<string>(null);
+  const [isModalOpen, setIsModalOpen] = useState(true);
 
   useEffect(() => {
-    fetchOrders().then(setOrders);
+    fetchOrders()
+      .then(setOrders)
+      .catch((error: any) => {
+        setError(error.message);
+      });
   }, []);
 
   return (
-    <ContainerBox title={"Received Orders"}>
-      <div className="flex flex-col gap-5 overflow-hidden ">
-        {orders ? (
-          [...orders].reverse().map((order) => (
-            <div key={order.id} className="collapse bg-base-200">
-              <input type="checkbox" />
-              <div className="collapse-title text-xl font-medium">
-                {order.studyProgram.educationInstitute.name} /{" "}
-                {order.studyProgram.name} / {order.internshipField} /{" "}
-                {order.studyYear} / {order.created_at.toLocaleDateString()}{" "}
-                (Click to open/close)
+    <>
+      <ContainerBox title={"Received Orders"}>
+        <div className="flex flex-col gap-5 overflow-hidden ">
+          {orders ? (
+            [...orders].reverse().map((order) => (
+              <div key={order.id} className="collapse bg-base-200">
+                <input type="checkbox" />
+                <div className="collapse-title text-xl font-medium">
+                  {order.studyProgram.educationInstitute.name} /{" "}
+                  {order.studyProgram.name} / {order.internshipField} /{" "}
+                  {order.studyYear} / {order.created_at.toLocaleDateString()}{" "}
+                  (Click to open/close)
+                </div>
+                <div className="collapse-content">
+                  <p>Number of students: {order.numStudents}</p>
+                  <p>Start week: {order.startWeek}</p>
+                  <p>End week: {order.endWeek}</p>
+                  <p>Comment: {order.comment.toString()}</p>
+                </div>
               </div>
-              <div className="collapse-content">
-                <p>Number of students: {order.numStudents}</p>
-                <p>Start week: {order.startWeek}</p>
-                <p>End week: {order.endWeek}</p>
-                <p>Comment: {order.comment.toString()}</p>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </ContainerBox>
+            ))
+          ) : error ? (
+            error === "No orders found in the internshipOrders table." ? (
+              <p>No orders found.</p>
+            ) : isModalOpen ? (
+              <ErrorModal
+                message={error}
+                setIsModalOpen={setIsModalOpen}
+              ></ErrorModal>
+            ) : null
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </ContainerBox>
+    </>
   );
 }
--- src/app/components/DynamicTable.tsx
+++ src/app/components/DynamicTable.tsx
@@ -7,6 +7,7 @@ import Add from "@/../public/Icons/add";
 import { PageResponse } from "../_models/pageinition";
 import { useSearchParams } from "next/navigation";
 import ContainerBox from "./ContainerBox";
+import ErrorModal from "./ErrorModal";
 
 type DynamicTableProps = {
   tableName: string;
@@ -43,7 +44,7 @@ const DynamicTable: React.FC<DynamicTableProps> = ({
     const isSelected = selectedRows.includes(row);
     if (isSelected) {
       setSelectedRows(
-        selectedRows.filter((selectedRow) => selectedRow !== row),
+        selectedRows.filter((selectedRow) => selectedRow !== row)
       );
     } else {
       setSelectedRows([...selectedRows, row]);
@@ -276,39 +277,10 @@ const DynamicTable: React.FC<DynamicTableProps> = ({
       </ContainerBox>
       {/**Delete Error modal */}
       {isModalOpen && (
-        <dialog
-          className="fixed inset-0 z-10 overflow-y-auto bg-base-100 rounded-lg"
-          open
-          aria-labelledby="Delete Error"
-        >
-          <div className="px-4 pt-5 pb-4 ">
-            <div className="sm:flex sm:items-start">
-              <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-error sm:mx-0 sm:h-10 sm:w-10">
-                {/* Icon or image can be added here */}
-              </div>
-              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3
-                  className="text-lg leading-6 font-medium text-base-content"
-                  id="Delete Error"
-                >
-                  Error
-                </h3>
-                <div className="mt-2">
-                  <p className="text-sm text-base-content">{error}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button
-              type="button"
-              className="btn btn-info"
-              onClick={() => setIsModalOpen(false)}
-            >
-              Close
-            </button>
-          </div>
-        </dialog>
+        <ErrorModal
+          message={error}
+          setIsModalOpen={setIsModalOpen}
+        ></ErrorModal>
       )}
     </>
   );

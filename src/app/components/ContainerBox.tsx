9566f273a981d1458d72adc07ad95e8fc407980b
Dardnos <adne.o94@gmail.com>
2024-04-25 15:17:01 +0200

    Add className prop to ContainerBox component

--- src/app/components/ContainerBox.tsx
+++ src/app/components/ContainerBox.tsx
@@ -1,8 +1,8 @@
-const ContainerBox = ({ children, title = "" }) => {
+const ContainerBox = ({ children, title = "", className = "" }) => {
   return (
-    <div className="flex flex-col p-10 ">
+    <div className={`flex flex-col p-10 ${className}`}>
       {title && <h1>{title}</h1>}
-      <div className="flex flex-col justify-center items-center bg-neutral text-neutral-content p-5 rounded-2xl gap-5">
+      <div className="flex flex-col h-full justify-center items-center bg-neutral text-neutral-content p-5 rounded-2xl gap-5">
         {children}
       </div>
     </div>

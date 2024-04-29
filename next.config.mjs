8bfbc654ad26b66704f80666c56e1f1effccbcf8
JeyKey09 <mathias-jk@live.no>
2024-04-29 15:19:17 +0200

    Add knex, mysql and daisyui in the server

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    serverComponentsExternalPackages: ["knex", "mysql2", "daisyui"],
+    outputFileTracingIncludes: {
+      "/src/knex": ["./src/knex/**/*.ts"],
+    },
+  },
 };
 
 export default nextConfig;

02ee31e9cfe7ef0a16b4059b180052794cfd0d8e
JeyKey09 <mathias-jk@live.no>
2024-04-30 09:14:48 +0200

    pipeline changes + removing of extension checks

--- src/knex/knexfile.ts
+++ src/knex/knexfile.ts
@@ -2,13 +2,6 @@ import type { Knex } from "knex";
 import path from "path";
 import { env } from "node:process";
 
-// Update with your config settings.
-const extension =
-  process.env.NODE_CONFIG_ENV === "production" ||
-  process.env.NODE_CONFIG_ENV === "staging"
-    ? "js"
-    : "ts";
-
 export const config: { [key: string]: Knex.Config } = {
   development: {
     client: "mysql2",
@@ -17,13 +10,11 @@ export const config: { [key: string]: Knex.Config } = {
     },
     migrations: {
       directory: path.join(__dirname, "migrations"),
-      extension: extension,
+      extension: "ts",
       tableName: "knex_migrations",
-      loadExtensions: [`.${extension}`],
     },
     seeds: {
       directory: path.join(__dirname, "seeds"),
-      loadExtensions: [`.${extension}`],
     },
   },
 
@@ -55,14 +46,12 @@ export const config: { [key: string]: Knex.Config } = {
     },
     migrations: {
       directory: path.join(__dirname, "migrations"),
-      extension: extension,
+      extension: "ts",
       tableName: "knex_migrations",
-      loadExtensions: [`.${extension}`],
     },
     //TODO: Should be removed in production
     seeds: {
       directory: path.join(__dirname, "seeds"),
-      loadExtensions: [`.${extension}`],
     },
   },
 };

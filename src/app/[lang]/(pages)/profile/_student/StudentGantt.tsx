2fad461473331f2ecfdfad35a1d83abb2f0682f1
JeyKey09 <mathias-jk@live.no>
2024-05-05 21:40:08 +0200

    Moved file since only student profile is gonna use it

--- src/app/[lang]/(pages)/profile/_student/StudentGantt.tsx
+++ src/app/[lang]/(pages)/profile/_student/StudentGantt.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Gantt from "../Gantt";
+import Gantt from "../../../../components/Gantt";
 import { getStudentAgreementGanttIntervals } from "@/app/[lang]/(pages)/profile/_student/student";
 
 /**

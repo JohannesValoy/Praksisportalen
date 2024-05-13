343210ac6dfb1b838702234df9167a8208377c21
JeyKey09 <mathias-jk@live.no>
2024-05-13 22:43:16 +0200

    Moved duplicated code to a very specific function

--- src/app/[lang]/(pages)/profile/_student/student.tsx
+++ src/app/[lang]/(pages)/profile/_student/student.tsx
@@ -25,18 +25,7 @@ export async function getStudentAgreementGanttIntervals(
   studentID: string,
   days: number = 6,
 ): Promise<GanttProp[]> {
-  date.setDate(date.getDate() - date.getDay());
-  const startDate = new Date(date);
-  startDate.setHours(0);
-  startDate.setMinutes(0);
-  startDate.setSeconds(0);
-  startDate.setMilliseconds(0);
-  date.setDate(date.getDate() + (days > 0 ? days : 6));
-  const endDate = new Date(date);
-  startDate.setHours(23);
-  startDate.setMinutes(59);
-  startDate.setSeconds(59);
-  startDate.setMilliseconds(999);
+ const [startDate, endDate] = getIntervalBetweenStartOfWeekAndTotalOffsetDays(date, days)
   return await DBClient.transaction(async (trx) => {
     const agreements = await trx
       .from("internshipAgreements")
